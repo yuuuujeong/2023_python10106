d08955ffb6711a77cce97226192f550f5603580f
yuuuujeong <105706810+yuuuujeong@users.noreply.github.com>
2023-03-08 19:43:58 +0900

    Add files via upload

---
+++ 2023_python10106/lec01/hello.py
@@ -0,0 +1,8 @@
+temp_c = 30
+temp_f = temp_c * (180/100) + 32
+print( "{} ℃=> {} ℉".format(temp_c, temp_f))
+
+temp_c = 0
+temp_f = temp_c * (180/100) + 32
+print( "{} ℃=> {} ℉".format(temp_c, temp_f))
+
---
+++ 2023_python10106/lec01/hello.py_02.py
@@ -0,0 +1,4 @@
+weight = 57
+height = 1.65
+BMI = weight / height ** 2
+print(BMI)
